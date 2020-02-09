f3a801c1ae77569215a02ab8f2d5483fa0c48e3a
Alaattin Bedir <alaattinbedir@gmail.com>
2020-02-09 15:42:28 +0300

    Initial Commit

---
+++ TodayInHistory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayInHistory
+//
+//  Created by mac on 9.02.2020.
+//  Copyright © 2020 Alaattin Bedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
